e9e0a292133c85a2051ba9cbd1dd438c82a9437d
haridass <haridass.pyallore@excelindia.com>
2015-05-21 17:37:29 +0530

    remove hard coded urls

--- WebContent/scripts/config.js
+++ WebContent/scripts/config.js
@@ -1,14 +1,18 @@
 (function(ns) {
 	
-    ns.host = window.location.protocol + "//" + window.location.host + "/mytest";
+	ns.domain = window.location.protocol + "//" + window.location.host;
+	
+    ns.host = ns.domain + "/mytest";
 	ns.homeEndpoint = "/home/questionbanks";
 	ns.homeTestGen = "/questionBanks";
 	ns.welcome = "/welcome";
 	
 	ns.viaPILogin = true;
-	ns.signinUrl = "http://localhost.dev-prsn.com:8080/evalu8-ui/#/signin";
-	ns.homeUrl = "http://localhost.dev-prsn.com:8080/evalu8-ui/#/home/questionbanks";
-	ns.welcomeUrl = "http://localhost.dev-prsn.com:8080/evalu8-ui/#/welcome";
+	
+	ns.signinUrl = ns.domain + "/evalu8-ui/#/signin";
+	ns.homeUrl = ns.domain + "/evalu8-ui/#/home/questionbanks";
+	ns.welcomeUrl = ns.domain + "/evalu8-ui/#/welcome";
+	
 	ns.loginGraceTimeSeconds = 2; 
 	ns.welcomeLoginCount = 5;
 		
