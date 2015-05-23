c865b7de5d17b3447c0cdff4c4fd7c686f4d197a
Basavanna S <basava.ahalya@gmail.com>
2015-05-23 21:17:29 +0530

    Thinking indicator issue fixed

--- WebContent/scripts/controllers/home-controller.js
+++ WebContent/scripts/controllers/home-controller.js
@@ -3,4 +3,6 @@
 angular.module('evalu8Demo')
   .controller('HomeController', function ($scope) {
     
-  });
+  });angular.module('blockUI').run(['$templateCache', function($templateCache){
+	  $templateCache.put('angular-block-ui/angular-block-ui.ng.html', '<div class=\"block-ui-overlay\"></div><div class=\"block-ui-message-container\" aria-live=\"assertive\" aria-atomic=\"true\"><div class=\"block-ui-message\" ng-class=\"$_blockUiMessageClass\"><div class="fa fa-spinner fa-pulse "></div></div></div>');
+  }]);
