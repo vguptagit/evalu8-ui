28c497a7b0a3bc8f66fa68f23cf38f4ca75d50be
Ravikumar GH <ravikumar.gh@excelindia.com>
2015-06-10 18:46:59 +0530

    issue fixed : "Pencil and Archive" icons are not displayed for the first test created and on closing the test tab

--- WebContent/scripts/controllers/myTests-controller.js
+++ WebContent/scripts/controllers/myTests-controller.js
@@ -497,6 +497,7 @@ angular.module('e8MyTests')
 	                        		if(testTab.testId == test.guid) {
 	                        			test.showEditIcon = false;
 	                        			test.showArchiveIcon = false;
+	                        			testTab.treeNode = test;
 	                        		}
 	                        	});
                         	}
