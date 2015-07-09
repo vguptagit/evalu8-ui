36b222670460520a7df291e25696a34967c5ce0b
Basavanna S <basava.ahalya@gmail.com>
2015-07-09 09:58:03 +0530

    Issue fixed: Delay in displaying alert messages in question bank tab

--- WebContent/scripts/controllers/questionBanks-controller.js
+++ WebContent/scripts/controllers/questionBanks-controller.js
@@ -943,7 +943,7 @@ angular
 									EnumService.RESOURCES_TABS.questionbanks);
 
 							$scope.openUserSettings = function(step) {
-								$scope.isAddQstbBankClicked=true;
+								$scope.isAddQstBankClicked=true;
 								$modal
 										.open({
 											templateUrl : 'views/usersettings/usersettingsWizard.html',
@@ -1045,8 +1045,8 @@ angular
 							}
 
 							$scope.validateSearch = function(){
-								$scope.isSimpleSearchClicked=true;
 								if($scope.selectedBooks.length == 0){
+									$scope.isSimpleSearchClicked=true;
 									$scope.showWaitingForAutoComplete=false;
 									$scope.selectedContainer="";
 									$scope.IsConfirmation = false;
@@ -1232,13 +1232,14 @@ angular
 							$scope.isSaveDisabled=true;
 							$scope.isAdvancedSearchClicked=false;
 							$scope.isSimpleSearchClicked=false;
-							$scope.isAddQstbBankClicked=false;
+							$scope.isAddQstBankClicked=false;
 							
 							$scope.openAdvancedSearch = function() {
 								$scope.isAdvancedSearchClicked=true;
 								if($scope.validateSearch()){
 									if (!$scope.showAdvancedSearch) {
 										$scope.showAdvancedSearch = true;
+										$scope.isAdvancedSearchClicked=false;
 									} else {
 										$scope.showAdvancedSearch = false;
 									}
