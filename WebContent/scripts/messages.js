32a99e7b9b3be97920620523068d861fe5ebbb2b
vbhagga <ganapati.bhagwat@excelindia.com>
2015-10-12 17:20:23 +0530

    Changing the folder restore message.

--- WebContent/scripts/messages.js
+++ WebContent/scripts/messages.js
@@ -37,7 +37,7 @@
 
 	ns.validation = {
 		duplicateTestTitle : "A test already exists with this name.",
-		duplicateFolderTitle : "A folder already exists with this name."
+		duplicateFolderTitle : "Test(s) already exists with the same name in the folder."
 	};
 	
 	ns.warning = {	
