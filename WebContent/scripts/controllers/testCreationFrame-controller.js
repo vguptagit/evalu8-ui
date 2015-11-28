8ad1e4175462a685ea1e620f813e395f98031679
Ravikumar GH <ravikumar.gh@excelindia.com>
2015-11-28 19:21:33 +0530

    resolved conflict

--- WebContent/scripts/controllers/testCreationFrame-controller.js
+++ WebContent/scripts/controllers/testCreationFrame-controller.js
@@ -1460,14 +1460,6 @@ angular
                                     QTI.initialize();
                                     $scope.tests[$scope.sharedTabService.currentTabIndex].isTestWizard = false;
                                     $scope.sharedTabService.isTestWizardTabPresent = false;
-                                    for (var i = 0; i < test.criterias.length; i++) {
-                                        test.criterias[i].treeNode.showTestWizardIcon = true;
-                                        if (test.criterias[i].treeNode.nodes) {
-                                            for (var j = 0; j < test.criterias[i].treeNode.nodes.length; j++) {
-                                                test.criterias[i].treeNode.nodes[j].showTestWizardIcon = true;
-                                            }
-                                        }
-                                    } 
                                     test.criterias=[];
                                     if (test.saveMode === EnumService.SAVE_MODE.SaveAs) {
                                         test.testId = null;
